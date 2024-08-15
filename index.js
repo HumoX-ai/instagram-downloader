7fb6da1f35ca01b6cbc21465ec932631f4710e58
Humoyun Abduqodirov <abduodirovhumoyun@gmail.com>
2024-08-15 16:15:28 +0500

    5

--- index.js
+++ index.js
@@ -68,12 +68,12 @@ bot.on("message:text", async (ctx) => {
 bot.start();
 
 // Express serverini yaratish
-// const app = express();
-// app.use(express.json());
+const app = express();
+app.use(express.json());
 
-// app.use(webhookCallback(bot, "express"));
+app.use(webhookCallback(bot, "express"));
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
